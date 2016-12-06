7b10d83e5c66d46456ca12e154853b755ed797e4
KentonJiang <jiangzhenfei1997@hotmail.com>
2016-12-06 09:58:37 +0800

    Initial Commit

---
+++ huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffman
+//
+//  Created by Jiang Zhenfei on 2016/12/6.
+//  Copyright © 2016年 姜震飞. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
